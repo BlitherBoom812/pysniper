9e2e63b8e210d63656cbfeed2be041ca4a6536ba
Guoyun Tian <guoyun812@qq.com>
2024-03-20 10:56:53 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# from setuptools import setup
+# from setuptools import find_packages
+
+
+# VERSION = '0.1.0'
+
+# setup(
+#     name='pysniper',  # package name
+#     version=VERSION,  # package version
+#     description='Track changes to every line of code and trigger customizable callback events.',  # package description
+#     packages=find_packages(),
+#     zip_safe=False,
+# )
+
+from setuptools import setup
+
+setup()
